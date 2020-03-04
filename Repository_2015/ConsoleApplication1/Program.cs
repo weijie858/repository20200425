c739d303a0e50fa4410353912a09767140a27c23
317543501@qq.com <317543501@qq.com>
2020-03-04 14:58:08 +0800

    C#中string.Empty、""和null 之间的区别

--- Repository_2015/ConsoleApplication1/Program.cs
+++ Repository_2015/ConsoleApplication1/Program.cs
@@ -6,11 +6,28 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
+    //C#中string.Empty、""和null 之间的区别 
+    //https://blog.csdn.net/henulwj/article/details/7830615
     class Program
     {
         static void Main(string[] args)
         {
+            string str = null;
+            string str2 = string.Empty;
+          //  str = str.ToString();//未将对象引用设置到对象的实例
+            str2 = str2.ToString();
             Console.WriteLine("台式测试");
+            Console.WriteLine(str);
+            Console.WriteLine(str2);
+            Console.ReadKey();
         }
     }
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("台式测试");
+    //        Console.ReadKey();
+    //    }
+    //}
 }
