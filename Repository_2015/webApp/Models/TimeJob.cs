c90c884d5508e2fa532313e39259f47f92e0cb40
e <317543501@qq.com>
2020-03-19 22:14:12 +0800

    quartz 记录

---
+++ Repository_2015/webApp/Models/TimeJob.cs
@@ -0,0 +1,17 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webApp.Models
+{
+    public class TimeJob : IJob
+    {
+      //  https://www.cnblogs.com/best/p/7658573.html
+        public void Execute(IJobExecutionContext context)
+        {
+            System.IO.File.AppendAllText(@"d:\Quartz.txt", DateTime.Now + Environment.NewLine);
+        }
+    }
+}
