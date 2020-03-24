a8def8d1e3b6fbda9ee041f2f429c655bd30aecd
e <317543501@qq.com>
2020-03-24 11:17:38 +0800

    笔记

--- Repository_2015/ConsoleOracleEFTest/Program.cs
+++ Repository_2015/ConsoleOracleEFTest/Program.cs
@@ -10,6 +10,14 @@ namespace ConsoleOracleEFTest
     {
         static void Main(string[] args)
         {
+            List<EMP> list = null;
+            using (Entities db = new Entities())
+            {
+                list = db.EMPs.Select(x => x).ToList();
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        
         }
     }
 }
