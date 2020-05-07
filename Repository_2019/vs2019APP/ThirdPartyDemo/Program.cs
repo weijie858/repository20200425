79bdb41456f9669ce9cc5d6102d2848f2d93295d
weijie858 <317543501@QQ.com>
2020-05-07 21:11:49 +0800

    第三方ClientCredential模式调用

---
+++ Repository_2019/vs2019APP/ThirdPartyDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using IdentityModel.Client;
+
+namespace ThirdPartyDemo
+{
+    class Program
+    {
+        //第三方ClientCredential模式调用
+        static void Main(string[] args)
+        {
+            var diso = DiscoveryClient.GetAsync("http://localhost:5000").Result;
+            if (diso.IsError)
+            {
+                Console.WriteLine(diso.Error);
+            }
+            var tokenClient = new TokenClient(diso.TokenEndpoint, "client", "secret");
+        var tockenResponse=    tokenClient.RequestClientCredentialsAsync("api").Result;
+            if (tockenResponse.IsError)
+            {
+                Console.WriteLine(tockenResponse.Error);
+            }
+            else
+            {
+                Console.WriteLine(tockenResponse.Json);
+            }
+            var httpClient = new HttpClient();
+            httpClient.SetBearerToken(tockenResponse.AccessToken);
+            var response = httpClient.GetAsync("http://localhost:5001/api/values").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+     
+            Console.ReadLine();
+        }
+    }
+}
